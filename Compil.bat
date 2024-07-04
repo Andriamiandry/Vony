f60642341888ec36e2cdd46cccbb6ec0285e4ad4
Andriamiandry <155827763+Andriamiandry@users.noreply.github.com>
2024-07-04 05:51:57 +0200

    Done

--- Compil.bat
+++ Compil.bat
@@ -20,17 +20,19 @@ jar cf GetAnnotation.jar mg
 jar cf Param.jar mg
 jar cf Post.jar mg
 jar cf RequestBody.jar mg
+jar cf MySession.jar mg
 
 :: Déplacer les fichiers JAR créés dans le répertoire souhaité
-move FrontController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move AnnotationController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move AnnotationMethode.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move Mapping.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move ModelView.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move GetAnnotation.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move Param.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move Post.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
-move RequestBody.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 07\lib"
+move FrontController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move AnnotationController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move AnnotationMethode.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move Mapping.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move ModelView.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move GetAnnotation.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move Param.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move Post.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move RequestBody.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move MySession.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
 
 :: Attendre 60 secondes
 timeout /t 60
