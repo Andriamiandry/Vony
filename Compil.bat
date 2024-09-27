de24960ebd583620f8052c159287abdc9f64ff7c
Andriamiandry <155827763+Andriamiandry@users.noreply.github.com>
2024-09-27 05:25:22 +0200

    RestApi

--- Compil.bat
+++ Compil.bat
@@ -3,6 +3,7 @@ setlocal
 
 :: Définir le chemin vers le répertoire où se trouve votre classe FrontController
 set DIR=D:\Studie\L2 -------S4--------\Mr Naina\Vony
+set DEPLOY = D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 09\lib
 
 :: Déplacer dans le répertoire contenant votre classe FrontController
 cd "%DIR%"
@@ -20,19 +21,21 @@ jar cf GetAnnotation.jar mg
 jar cf Param.jar mg
 jar cf Post.jar mg
 jar cf RequestBody.jar mg
-jar cf MySession.jar mg
+jar cf CustomSession.jar mg
+jar cf Restapi.jar mg
 
 :: Déplacer les fichiers JAR créés dans le répertoire souhaité
-move FrontController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move AnnotationController.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move AnnotationMethode.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move Mapping.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move ModelView.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move GetAnnotation.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move Param.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move Post.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move RequestBody.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
-move MySession.jar "D:\Studie\L2 -------S4--------\Mr Naina\TestSprint 08\lib"
+move FrontController.jar "%DEPLOY%"
+move AnnotationController.jar "%DEPLOY%"
+move AnnotationMethode.jar "%DEPLOY%"
+move Mapping.jar "%DEPLOY%"
+move ModelView.jar "%DEPLOY%"
+move GetAnnotation.jar "%DEPLOY%"
+move Param.jar "%DEPLOY%"
+move Post.jar "%DEPLOY%"
+move RequestBody.jar "%DEPLOY%"
+move CustomSession.jar "%DEPLOY%"
+move Restapi.jar "%DEPLOY%"
 
 :: Attendre 60 secondes
 timeout /t 60
--- CustomSession.java
+++ CustomSession.java
@@ -2,10 +2,10 @@ package mg.itu.prom16;
 
 import jakarta.servlet.http.HttpSession;
 
-public class MySession {
+public class CustomSession {
     HttpSession session;
 
-    public MySession(HttpSession session) {
+    public CustomSession(HttpSession session) {
         this.session = session;
     }
 
